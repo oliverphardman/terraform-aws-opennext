39576b1ad96d10afeee0e0cdc3c4191584e0b228
Oliver Hardman <hello@oliverhardman.com>
2026-04-11 19:49:24 +0100

    fix: cache table key syntax

--- modules/opennext-cache-table/dynamodb.tf
+++ modules/opennext-cache-table/dynamodb.tf
@@ -1,21 +1,34 @@
 resource "aws_dynamodb_table" "this" {
   name         = "${var.slug}Cache"
   billing_mode = var.dynamodb_cache_billing_mode
-  hash_key     = "Path"
-  range_key    = "RevalidatedAt"
+  hash_key     = "tag"
+  range_key    = "path"
 
   attribute {
-    name = "Path"
+    name = "tag"
     type = "S"
   }
 
   attribute {
-    name = "RevalidatedAt"
-    type = "N"
+    name = "path"
+    type = "S"
+  }
+
+  global_secondary_index {
+    name = "revalidate"
+    key_schema {
+      attribute_name = "path"
+      key_type       = "HASH"
+    }
+    key_schema {
+      attribute_name = "revalidatedAt"
+      key_type       = "RANGE"
+    }
+    projection_type = "ALL"
   }
 
   ttl {
-    attribute_name = "ExpireAt"
+    attribute_name = "revalidatedAt"
     enabled        = true
   }
 
